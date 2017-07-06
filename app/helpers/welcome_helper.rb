f8b7c2d79cf222cf778f050ae2d89bb21281d434
amansawhney <amansawhney@sawhneygames.com>
2017-07-06 11:50:40 -0400

    Made the jokes more punny

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,10 +1,10 @@
 module WelcomeHelper
   def scrape()
     links = []
-    source = open("https://twitter.com/shittyjokes?lang=en")
+    source = open("https://twitter.com/Puns_Only?lang=en")
     page = Nokogiri::HTML(source)
     page.css('div.js-tweet-text-container > p').each do |link|
-        links << { content: link.content}
+        links << { content: link.content.gsub("\n", ' ')}
     end
     return links[rand(links.size)][:content]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622204401) do
+ActiveRecord::Schema.define(version: 20170629235347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "films", force: :cascade do |t|
+    t.string "name"
+    t.string "developer"
+    t.string "dilution"
+    t.string "iso"
+    t.float "time"
+    t.string "temp"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "phone"
